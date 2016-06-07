963505d9901ffd51e13264b59566d3c8cb11dec6
cosa65 <mfosco65@gmail.com>
2016-06-07 18:59:13 -0300

    blablabla

---
+++ test/ploteoPuntitos.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+x = np.arange(0.0049, 0.0012, 0.0092, 0.214, 0.0138, 0.1609, 0.0058, 0.0105, 0.0084)
+y = np.arange(2,3,4,5,6,7,8,9,12)
+plt.ylabel('Diferencias en saltos (valor absoluto)')
+plt.xlabel('TTl de Saltos')
+plt.show()
